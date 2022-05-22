c6900f97c4ff85bf293397db1f912feef85cdef1
Dandre Bell <djbell1999@comcast.net>
2022-05-22 01:42:21 -0400

    documentation

--- getBio.php
+++ getBio.php
@@ -5,8 +5,12 @@
 
 <?php
     cors();
+    //sql statement will be set to production on host
     $sql = "select bioText from bio where deploymentStatus = 'test';";
+    //send query to database
     $result = mysqli_query($conn, $sql);
+    //store response in $row
     $row = mysqli_fetch_assoc($result);
+    //return element
     echo $row['bioText']
 ?>
